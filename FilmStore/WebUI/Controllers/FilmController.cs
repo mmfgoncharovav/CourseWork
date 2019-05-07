03e699f6a714b2028f7359b3dfa0e8cd97db4527
Alexandr <goncharovavmmf@gmail.com>
2019-05-07 16:48:26 +0300

    First part

---
+++ FilmStore/WebUI/Controllers/FilmController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Abstract;
+using Domain.Entities;
+
+namespace WebUI.Controllers
+{
+    public class FilmController : Controller
+    {
+        private IFilmRepository repository;
+        public FilmController(IFilmRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult List()
+        {
+            return View(repository.Films);
+        }
+    }
+}
